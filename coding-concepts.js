b1b746495c40990709556e47bcb587ebf77286a2
Derek Maly <dmaly18@gmail.com>
2023-10-09 11:15:41 -0700

    completed

--- coding-concepts.js
+++ coding-concepts.js
@@ -7,49 +7,49 @@
 // There is no need to change any of the code.
 
 // --------------------INSTRUCTOR EXAMPLE: What will this log?
-const colors = ["tangerine", "magenta", "lilac", "daffodil"]
-// console.log(colors.push("indigo"))
+//const colors = ["tangerine", "magenta", "lilac", "daffodil"]
+//console.log(colors.push("indigo"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 5
+// b) Verify and explain: My answer was correct because when push was called on it added indigo to the string adding a 5th data type.
 
 // --------------------1) What will this log?
 
-const cohort = "LEARN 2023"
-// console.log(cohort.length)
+//const cohort = "LEARN 2023"
+//console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:9
+// b) Verify and explain: The correct answer was 10 I did not include the space inside the string.
 
 // --------------------2) What will this log?
 
-const greeting = "Hello World!"
-// console.log(greeting[4])
+//const greeting = "Hello World!"
+//console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: My answer was correct because when the greeting was called [4] the o is the 4th letter inside the string.
 
 // --------------------3) What will this log?
 
-const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
-// console.log(languages[index])
+//const languages = ["JavaScript", "Ruby", "Python", "C++"]
+//const index = 1
+//console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: calling on a index the count starts at zero putting Ruby in spot 1 inside the string.
 
 // --------------------4) What will this log?
 
-const weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+//const weekendDays = ["saturday", "sunday"]
+//console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:"SATURDAY", "SUNDAY"
+// b) Verify and explain: I got the answer wrong, code came up as error the terminal is saying toUpperCase is not a function.
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:4
+// b) Verify and explain: I got the answer wrong the terminal is telling me that the answer is "number"
